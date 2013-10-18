695da99ac03e583dd2667e6551e3af7936ad3a2e
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-17 22:41:36 -0500

    tweets are streaming real time

--- app/assets/javascripts/driver.js
+++ app/assets/javascripts/driver.js
@@ -2,8 +2,8 @@ var map = undefined;
 $(document).ready(function(){
   map = loadMap();
   var marker;
-  getTweets();
-  events(map);
+  // getTweets();
+  // events(map);
   
   var dispatcher = new WebSocketRails('localhost:3000/websocket');
 
@@ -11,15 +11,13 @@ $(document).ready(function(){
   //   console.log("hello");
   // }
 
-  dispatcher.on_open = function(asd){
-    console.log(asd);
-  }
+
 
   
   dispatcher.trigger("events.tweets")
 
   dispatcher.bind("events.success", function(message){
-    console.log(message);
+     convertTweetsToMapObjects(message);
   })
   // dispatcher.bind('tasks.create_success', function(task) {
   // console.log('successfully created ' + task.name);
